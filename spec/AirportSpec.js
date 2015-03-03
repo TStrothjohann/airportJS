b34c1363fb1e5fcc2a00936de72d5886b655fc66
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-03 17:15:19 +0000

    go get zee germein party boy

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,11 +1,21 @@
 describe('Airport', function(){
 
+  var plane;
+  airport = new Airport()
 
+  beforeEach(function(){
+    plane = jasmine.createSpyObj('plane', ['land', 'takeOff'])
+  });
 
   it('can dock a plane', function(){
-    airport = new Airport()
-    airport.dock(plane)
+    airport.dock(plane);
     expect(airport.planes).toEqual([plane])
+  });
+
+  it('can undock a plane', function(){
+    airport.dock(plane);
+    airport.undock(plane);
+    expect(airport.planes).toEqual([]);
   });
 
 
